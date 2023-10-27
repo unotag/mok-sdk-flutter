49f48b5aacc0d37d7a77c6fe40464a8bbf0d27d5
sohelyadav <sohelyadav@gmail.com>
2023-10-28 02:19:54 +0530

    update: mok sdk version

--- android/src/main/kotlin/com/unomok/mokone/MokoneApplication.kt
+++ android/src/main/kotlin/com/unomok/mokone/MokoneApplication.kt
@@ -1,7 +1,7 @@
 package com.unomok.mokone
 
-import MokLogger
 import com.unotag.mokone.MokSDK
+import com.unotag.mokone.utils.MokLogger
 import io.flutter.app.FlutterApplication
 
 class MokoneApplication : FlutterApplication() {
@@ -9,7 +9,7 @@ class MokoneApplication : FlutterApplication() {
     override fun onCreate() {
         super.onCreate()
         val mokSDK = MokSDK.getInstance(applicationContext)
-        mokSDK.initMokSDK()
+        mokSDK.initMokSDK(isProductionEvn = false)
         MokLogger.setLogLevel(MokLogger.LogLevel.DEBUG)
     }
 }
