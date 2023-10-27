7156e376848cc9f5f842c488b81dbcadcdd0dfc6
sohelyadav <sohelyadav@gmail.com>
2023-10-27 19:36:36 +0530

    feat: notification permission

--- src/main/kotlin/com/unomok/mokone/MokoneApplication.kt
+++ src/main/kotlin/com/unomok/mokone/MokoneApplication.kt
@@ -1,15 +1,13 @@
 package com.unomok.mokone
 
-import MokLogger
 import com.unotag.mokone.MokSDK
 import io.flutter.app.FlutterApplication
 
-class MokoneApplication : FlutterApplication() {
+open class MokoneApplication : FlutterApplication() {
 
     override fun onCreate() {
         super.onCreate()
         val mokSDK = MokSDK.getInstance(applicationContext)
-        mokSDK.initMokSDK()
-        MokLogger.setLogLevel(MokLogger.LogLevel.DEBUG)
+        mokSDK.initMokSDK(isDevelopmentEvn = false)
     }
 }
